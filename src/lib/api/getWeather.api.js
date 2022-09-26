9f44c391687d08148b2ba4f5fe664eb1434dc6b9
Jesus Aguiar <aguiarf.j16@gmail.com>
2022-09-26 12:23:57 -0300

    added .env

--- src/lib/api/getWeather.api.js
+++ src/lib/api/getWeather.api.js
@@ -1,7 +1,13 @@
 // @ts-nocheck
-import axios from "axios";
+import axios from 'axios';
 export const getWeather = async () => {
-	const locationResult = await axios.get('https://extreme-ip-lookup.com/json/?key=');
-	const weatherResult = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationResult.data.city}?unitGroup=metric&key=&contentType=json`);
+	const locationResult = await axios.get(
+		`https://extreme-ip-lookup.com/json/?key=${import.meta.env.VITE_LOCATION_KEY}`
+	);
+	const weatherResult = await axios.get(
+		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${
+			locationResult.data.city
+		}?unitGroup=metric&key=${import.meta.env.VITE_WEATHER_KEY}&contentType=json`
+	);
 	return weatherResult;
 };
