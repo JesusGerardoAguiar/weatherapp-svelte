8f327cd19f3542f985c5027e60739316e5d63b28
Jesus Aguiar <aguiarf.j16@gmail.com>
2022-09-30 14:28:21 -0300

    added headers to api call

--- src/lib/api/getWeather.api.js
+++ src/lib/api/getWeather.api.js
@@ -4,13 +4,22 @@ export const getWeather = async () => {
 	const headers = {
 		"Content-Type": "application/json",
 	  };
-	const locationResult = await axios.get(
-		`https://extreme-ip-lookup.com/json/?key=${import.meta.env.VITE_LOCATION_KEY}`
-	, {headers});
+
 	const weatherResult = await axios.get(
-		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${
-			locationResult.data.city
-		}?unitGroup=metric&key=${import.meta.env.VITE_WEATHER_KEY}&contentType=json`
+		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Montevideo?unitGroup=metric&key=${import.meta.env.VITE_WEATHER_KEY}&contentType=json`
 		, {headers});
 	return weatherResult;
 };
+
+
+// const headers = {
+// 	"Content-Type": "application/json",
+//   };
+// const locationResult = await axios.get(
+// 	`https://extreme-ip-lookup.com/json/?key=${import.meta.env.VITE_LOCATION_KEY}`
+// , {headers});
+// const weatherResult = await axios.get(
+// 	`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${
+// 		locationResult.data.city
+// 	}?unitGroup=metric&key=${import.meta.env.VITE_WEATHER_KEY}&contentType=json`
+// 	, {headers});
